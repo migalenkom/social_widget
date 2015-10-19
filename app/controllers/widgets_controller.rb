1a6e876fd0aacc67a7c5a6e1afe9f761c5bbf3e3
Mike M <m.mygalenko@svitla.com>
2015-10-19 12:00:28 +0300

    ajax create destroy

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -7,15 +7,7 @@ class WidgetsController < ApplicationController
     @widgets = current_user.widgets
     @result = []
     @widgets.each do |w|
-      result = case w.authorization.provider
-                 when 'facebook'
-                   @result <<   fetch_facebook_data(w.authorization)
-                 when 'twitter'
-                   @result << fetch_twitter_data(w.authorization)
-                 when 'google_oauth2'
-                   @result << fetch_google_data(w.authorization)
-                 else nil
-               end
+      @result << self.send("fetch_#{w.authorization.provider}_data",w)
     end
 
   end
@@ -28,8 +20,9 @@ class WidgetsController < ApplicationController
 
     @widget = current_user.widgets.new(widget_params)
     current_user.widgets << @widget
-
+    @created_widget = self.send("fetch_#{@widget.authorization.provider}_data",@widget)
     if @widget.save {
+
       respond_to do |format|
         format.html {redirect_to user_widgets_path(current_user), :notice => "Widget created"}
         format.js {}
@@ -62,17 +55,18 @@ class WidgetsController < ApplicationController
 
   private
 
-  def fetch_facebook_data(auth)
+  def fetch_facebook_data(widget)
 
+    auth = widget.authorization
     fb_api = Koala::Facebook::API.new(auth.token)
     fb_profile = fb_api.get_object("me?fields=name,picture,posts,link")
 
-    {provider: auth.provider, fb_profile:fb_profile}
+    {widget:widget, provider: auth.provider, fb_profile:fb_profile}
 
   end
 
-  def fetch_twitter_data(auth)
-
+  def fetch_twitter_data(widget)
+    auth = widget.authorization
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_KEY"]
       config.consumer_secret     = ENV["TWITTER_SECRET"]
@@ -82,17 +76,17 @@ class WidgetsController < ApplicationController
     tw_user = client.user(auth.uid.to_i)
     tw_timeline = client.user_timeline(auth.uid.to_i)
 
-    {provider: auth.provider, tw_user: tw_user, tw_timeline: tw_timeline }
+    {widget:widget, provider: auth.provider, tw_user: tw_user, tw_timeline: tw_timeline }
 
   end
 
-  def fetch_google_data(auth)
-
-       GooglePlus.access_token = auth.fresh_token
+  def fetch_google_oauth2_data(widget)
+    auth = widget.authorization
+    GooglePlus.access_token = auth.fresh_token
        googlePerson = GooglePlus::Person.get(auth.uid)
        googleActivity = GooglePlus::Activity.for_person(auth.uid)
 
-       {provider: auth.provider, google_persone: googlePerson, google_activity: googleActivity }
+       {widget:widget, provider: auth.provider, google_persone: googlePerson, google_activity: googleActivity }
   end
 
   def refresh_auth_token(auth)
