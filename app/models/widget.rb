c8cc87051ae8e33ea2619d63e36f268f09d4ee1c
Mike M <m.mygalenko@svitla.com>
2015-10-23 18:53:11 +0300

    add to first column by default

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,6 +1,17 @@
 class Widget < ActiveRecord::Base
 
+  before_create :set_col_position
+
   belongs_to :user
   belongs_to :authorization
+
+
+
+  def set_col_position
+
+    self.col =  1
+    self.position = Widget.count
+  end
+
 
 end
